999ab3e67339bd294c353ebff9c0893d11b6b1b5
nhoxzoner <64214215+nhoxzoner@users.noreply.github.com>
2021-11-27 17:25:42 +0700

    Add files via upload

---
+++ products.js
@@ -0,0 +1,103 @@
+const products = [
+  {
+    id: 1,
+    name: "iPhone 13 Pro Max",
+    brand: "Apple",
+    desc: "6.7-inch display",
+    price: 1099,
+    image:
+      "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-13-pro-family-hero?wid=470&hei=556&fmt=png-alpha&.v=1631220221000",
+  },
+  {
+    id: 2,
+    name: "iPhone 13 Pro",
+    brand: "Apple",
+    desc: "6.1-inch mini display",
+    price: 999,
+    image:
+      "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-13-pro-family-select?wid=1880&amp;hei=2224&amp;fmt=jpeg&amp;qlt=80&amp;.v=1631306948000",
+  },
+  {
+    id: 3,
+    name: "iPhone 13",
+    brand: "Apple",
+    desc: "6.1-inch display",
+    price: 799,
+    image:
+      "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-13-family-select-2021?wid=470&hei=556&fmt=jpeg&qlt=95&.v=1629842667000",
+  },
+  {
+    id: 4,
+    name: "iPhone 13 Mini",
+    brand: "Apple",
+    desc: "5.4-inch display",
+    price: 699,
+    image:
+      "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-13-mini-select-2021?wid=940&amp;hei=1112&amp;fmt=jpeg&amp;qlt=80&amp;.v=1629842691000",
+  },
+  {
+    id: 5,
+    name: "iPhone 12",
+    brand: "Apple",
+    desc: "6.1-inch display",
+    price: 699,
+    image:
+      "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-12-family-select-2021?wid=470&hei=556&fmt=jpeg&qlt=95&.v=1617135051000",
+  },
+  {
+    id: 6,
+    name: "iPhone 12 Mini",
+    brand: "Apple",
+    desc: "5.4-inch display",
+    price: 599,
+    image:
+      "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-12-mini-select-2021?wid=940&amp;hei=1112&amp;fmt=jpeg&amp;qlt=80&amp;.v=1617130318000",
+  },
+  {
+    id: 7,
+    name: "iPhone SE",
+    brand: "Apple",
+    desc: "5.4-inch display",
+    price: 999,
+    image:
+      "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-se-black-select-2020?wid=470&hei=556&fmt=png-alpha&.v=1586574260051",
+  },
+  {
+    id: 8,
+    name: "iPad Pro",
+    brand: "Apple",
+    desc: "12.9-inch display",
+    price: 1099,
+    image:
+      "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/ipad-pro-12-11-select-202104?wid=545&hei=550&fmt=jpeg&qlt=95&.v=1617067380000",
+  },
+  {
+    id: 9,
+    name: "iPad Air",
+    brand: "Apple",
+    desc: "10.9-inch display",
+    price: 879,
+    image:
+      "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/ipad-air-select-cell-green-202009?wid=470&hei=556&fmt=png-alpha&.v=1598650642000",
+  },
+  {
+    id: 10,
+    name: "Galaxy Z Fold3 5G",
+    brand: "Samsung",
+    desc: "7.6-inch display",
+    price: 1900,
+    image:
+      "https://images.samsung.com/is/image/samsung/p6pim/vn/2108/gallery/vn-galaxy-z-fold3-f926-5g-sm-f926bzsgxxv-474125554?$PD_SHOP_JPG$",
+  },
+  {
+    id: 11,
+    name: "Galaxy Tab S7 | S7+",
+    brand: "Samsung",
+    desc: "6.1-inch display",
+    price: 800,
+    image:
+      "https://images.samsung.com/is/image/samsung/p6pim/vn/sm-t875ndbaxxv/gallery/vn-galaxy-tab-s7-t875-343996-sm-t875ndbaxxv-413386798?$PD_SHOP_JPG$",
+  },
+];
+
+module.exports = products;
